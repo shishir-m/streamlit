7a430014582d61efc67564f0c73c51c8441a76e0
shishir-m <75567472+shishir-m@users.noreply.github.com>
2023-03-14 14:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 import streamlit as st
 import time
+import openai
+def get_response(prompt):
+    """Returns the response for the given prompt using the OpenAI API."""
+    completions = openai.Completion.create(
+             engine = "text-davinci-003",
+             prompt = prompt,
+         max_tokens = 512,
+        temperature = 0.7,
+    )
+    return completions.choices[0].text
+
+def handle_input(
+               input_str : str,
+    conversation_history : str,
+                 ):
+    """Updates the conversation history and generates a response using GPT-3."""
+    # Update the conversation history
+    conversation_history += f"Q: {input_str}\nA:"
+   
+    # Generate a response using GPT-3
+    message = get_response(conversation_history)
+
+    # Update the conversation history
+    conversation_history += f"{message}\n"
+    # Print the response
+    print(message)
+    
+    return conversation_history
 
 st.set_page_config(layout="wide")
-st.title('Logging in Text Box')
 
 # creating a placeholder for the fixed sized textbox
-logtxtbox = st.empty()
-logtxt = 'start'
-logtxtbox.text_area("Logging: ",logtxt, height = 500)
+txtbox = st.empty()
+txt = ''
+txtbox.text_area("Q:",txt, height = 500)
 
 end_of_loop = False
 counter = 1
 
 while (end_of_loop==False):
-
-    logtxt += 'Counter [' + str(counter) + '] \n'
-    logtxtbox.text_area("Logging: ", logtxt, height=500)
-
+    txt = txtbox.text_area("Enter your query:",txt, height = 500)
+    if st.button("Ask The Bot"):
+        output = handle_input(txt)
+    txt += output
+    txtbox.text_area("Enter your query:",txt, height=500)
     counter += 1
     if (counter > 100):
         end_of_loop = True
